e9e43e83216d516fe077b03bad9b7e352f226cbb
kodunaiff <kodunaiff@gmail.com>
2023-09-19 00:08:28 +0800

    Update active_passcards_view.py

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,11 +1,9 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
 
 
 def active_passcards_view(request):
 
-    active_passcards = Passcard.objects.all()
     context = {
         'active_passcards': Passcard.objects.filter(is_active=True),
     }
