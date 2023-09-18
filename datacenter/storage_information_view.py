0a5dd4907284e51d2ab6f1ec7f029e7e080f19d5
kodunaiff <kodunaiff@gmail.com>
2023-09-19 00:17:57 +0800

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,3 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from datacenter.functions_visits import get_duration, format_duration
